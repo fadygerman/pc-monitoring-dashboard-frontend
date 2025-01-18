7bb7d755f79063749e4ac03b5e9af5eadc150e18
Fady German <fady.german@hotmail.com>
2025-01-18 14:09:06 +0100

    feat: add Firebase Analytics integration and update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,21 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // ...your Firebase config...
+  apiKey: "AIzaSyDWsHpcSuXp9UA44chLceJbDpOvbKDZrmk",
+  authDomain: "pc-monitoring-dashboard.firebaseapp.com",
+  projectId: "pc-monitoring-dashboard",
+  storageBucket: "pc-monitoring-dashboard.firebasestorage.app",
+  messagingSenderId: "187613277439",
+  appId: "1:187613277439:web:6336579eed2ba55eb230ca",
+  measurementId: "G-02PB6QJH7N"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
